5df628cff93da6559aa14d18c1b80709593095cd
picopicodevil <picopicodevil@gmail.com>
2020-10-11 16:11:54 +0900

    Update: JSONを用いた一連の処理を追加

--- js/prot.js
+++ js/prot.js
@@ -1,6 +1,6 @@
 class Robot {
   constructor(x, y, direction, color) {
-    this._initialPosition = [Number(x), Number(y, 10)];
+    this._initialPosition = [Number(x), Number(y)];
     this._initialDirection = direction;
     this._initialColor = color;
     this._move = [];
@@ -27,12 +27,38 @@ class Robot {
     return this._move;
   }
 
+  get color() {
+    return this._color;
+  }
+
+  set move(array) {
+    this._move = array;
+  }
+
+  set initialDirection(direction) {
+    this._initialDirection = direction;
+  }
+
+  set color(array) {
+    this._color = array;
+  }
+
   setMove(index, move) {
     this._move[index] = move;
   }
 
-  setColor(index, color) {
-    this._color[index] = color;
+  setColor(turn, color) {
+    if (turn == 0) this._initialColor = color;
+    else this._color[turn - 1] = color;
+  }
+
+  getColor(turn) {
+    if (turn == 0) {
+      return this._initialColor;
+    } else {
+      if (turn > this._color.length) return this._color[this._color.length - 1];
+      else return this._color[turn - 1];
+    }
   }
 
   direction(turn) {
@@ -122,144 +148,110 @@ class Robot {
   }
 }
 
-function createTable(width, height, turn) {
+function createTable() {
   let table = document.createElement("table");
   table.setAttribute("class", "field");
   table.setAttribute("id", "field");
 
-  let dataPath = "csv/sample.csv";
-  let robots = [];
+  console.log(robot);
 
-  let request = new XMLHttpRequest(); // HTTPでファイルを読み込む
-  request.addEventListener("load", (event) => {
-    // ロードさせ実行
-    console.log("Load CSV.");
-    const str = event.target.responseText; // 受け取ったテキストを返す
-    // 最終的な二次元配列を入れるための配列
-    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
-
-    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
-    for (var i = 1; i < tmp.length - 1; i++) {
-      let str = [];
-      str[i] = tmp[i].split(",");
-      robots[i - 1] = new Robot(str[i][1], str[i][2], str[i][3], str[i][4]);
-      let index = 0;
-      for (var j = 5; j < str[i].length; j += 2) {
-        robots[i - 1].setMove(index, str[i][j]);
-        robots[i - 1].setColor(index, str[i][j + 1]);
-        index++;
-      }
-    }
-
-    console.log(robots);
-
-    for (let i = 0; i <= height; i++) {
-      let tr = document.createElement("tr");
-      for (let j = 0; j <= width; j++) {
-        if (robotExists(robots, turn, j, i) != -1) {
-          let robotNumber = robotExists(robots, turn, j, i);
-          let direction = robots[robotNumber].direction(turn).toLowerCase();
-          let td = document.createElement("td");
-          td.className = "robot_" + direction;
-          let id = "robot" + robotNumber;
-          td.id = id;
-          td.innerHTML = robotNumber;
-          robotNumber++;
-          tr.appendChild(td);
-        } else if (robotExists(robots, turn + 1, j, i) != -1) {
-          let robotNumber = robotExists(robots, turn + 1, j, i);
-          let direction = robots[robotNumber].direction(turn + 1).toLowerCase();
-          let td = document.createElement("td");
-          td.className = "next_robot_" + direction;
-          let id = "robot" + robotNumber;
-          td.id = id;
-          td.innerHTML = robotNumber;
-          robotNumber++;
-          tr.appendChild(td);
-        } else if (i == 0) {
-          // 1行目のtr要素の時
-          let th = document.createElement("th");
-          th.textContent = j;
-          th.className = "row_number";
-          tr.appendChild(th);
-        } else if (i == 1) {
-          let td = document.createElement("td");
-          if (j == 0) {
-            td.className = "column_number";
-            td.textContent = i;
-          } else if (j == 1) {
-            td.className = "cross_upper_left";
-          } else if (j == width) {
-            td.className = "cross_upper_right";
-          } else {
-            td.className = "cross_upper_middle";
-          }
-          tr.appendChild(td);
-        } else if (i == height) {
-          // 最終行のtr要素の時
-          let td = document.createElement("td");
-          if (j == 0) {
-            td.className = "column_number";
-            td.textContent = i;
-          } else if (j == 1) {
-            td.className = "cross_lower_left";
-          } else if (j == width) {
-            td.className = "cross_lower_right";
-          } else {
-            td.className = "cross_lower_middle";
-          }
-          tr.appendChild(td);
+  for (let i = 0; i <= height; i++) {
+    let tr = document.createElement("tr");
+    for (let j = 0; j <= width; j++) {
+      if (robotExists(turn, j, i) != -1) {
+        let robotNumber = robotExists(turn, j, i);
+        let direction = robot[robotNumber].direction(turn).toLowerCase();
+        let td = document.createElement("td");
+        td.className = "robot_" + direction;
+        let id = "robot" + robotNumber;
+        td.id = id;
+        td.innerHTML = robotNumber;
+        td.style.backgroundColor = robot[robotNumber].getColor(turn);
+        robotNumber++;
+        tr.appendChild(td);
+      } else if (robotExists(turn + 1, j, i) != -1) {
+        let robotNumber = robotExists(turn + 1, j, i);
+        let direction = robot[robotNumber].direction(turn + 1).toLowerCase();
+        let td = document.createElement("td");
+        td.className = "next_robot_" + direction;
+        let id = "next_robot" + robotNumber;
+        td.id = id;
+        td.innerHTML = robotNumber;
+        // td.style.backgroundColor = robot[robotNumber].getColor(turn + 1);
+        robotNumber++;
+        tr.appendChild(td);
+      } else if (i == 0) {
+        // 1行目のtr要素の時
+        let th = document.createElement("th");
+        th.textContent = j;
+        th.className = "row_number";
+        tr.appendChild(th);
+      } else if (i == 1) {
+        let td = document.createElement("td");
+        if (j == 0) {
+          td.className = "column_number";
+          td.textContent = i;
+        } else if (j == 1) {
+          td.className = "cross_upper_left";
+          td.id = "field" + j + "-" + i;
+        } else if (j == width) {
+          td.className = "cross_upper_right";
+          td.id = "field" + j + "-" + i;
         } else {
-          // 間の行のtr要素の時
-          let td = document.createElement("td");
-          if (j == 0) {
-            td.className = "column_number";
-            td.textContent = i;
-          } else if (j == 1) {
-            td.className = "cross_middle_left";
-            td.id = "field" + j + "-" + i;
-          } else if (j == width) {
-            td.className = "cross_middle_right";
-          } else {
-            td.className = "cross_middle";
-          }
-          tr.appendChild(td);
+          td.className = "cross_upper_middle";
+          td.id = "field" + j + "-" + i;
         }
+        tr.appendChild(td);
+      } else if (i == height) {
+        // 最終行のtr要素の時
+        let td = document.createElement("td");
+        if (j == 0) {
+          td.className = "column_number";
+          td.textContent = i;
+        } else if (j == 1) {
+          td.className = "cross_lower_left";
+          td.id = "field" + j + "-" + i;
+        } else if (j == width) {
+          td.className = "cross_lower_right";
+          td.id = "field" + j + "-" + i;
+        } else {
+          td.className = "cross_lower_middle";
+          td.id = "field" + j + "-" + i;
+        }
+        tr.appendChild(td);
+      } else {
+        // 間の行のtr要素の時
+        let td = document.createElement("td");
+        if (j == 0) {
+          td.className = "column_number";
+          td.textContent = i;
+        } else if (j == 1) {
+          td.className = "cross_middle_left";
+          td.id = "field" + j + "-" + i;
+        } else if (j == width) {
+          td.className = "cross_middle_right";
+          td.id = "field" + j + "-" + i;
+        } else {
+          td.className = "cross_middle";
+          td.id = "field" + j + "-" + i;
+        }
+        tr.appendChild(td);
       }
-      table.appendChild(tr);
     }
-    document.getElementById("field-table").appendChild(table);
+    table.appendChild(tr);
+  }
+  document.getElementById("field-table").appendChild(table);
 
-    for (let i = 0; i < robots.length; i++) {
-      let id = "robot" + i;
-      let elem = document.getElementById(id);
-      if (elem != null) {
-        elem.addEventListener(
-          "mouseover",
-          function (event) {
-            event.target.style.backgroundColor = "black";
-            console.log("MouseOver");
-            setTimeout(function () {
-              event.target.style.backgroundColor = "";
-            }, 500);
-          },
-          false
-        );
-      }
-    }
-  });
-  console.log("%s", dataPath);
-  request.open("GET", dataPath, true); // csvのパスを指定
-  request.send();
+  setContextMenu();
 }
 
 function drawField() {
   let field = document.getElementById("field");
   if (field != null) field.remove();
 
-  let turn = Number(document.getElementById("turn-value").value);
-  let x = Number(document.getElementById("field-width-value").value);
-  let y = Number(document.getElementById("field-height-value").value);
+  turn = Number(document.getElementById("turn-value").value);
+  width = Number(document.getElementById("field-width-value").value);
+  height = Number(document.getElementById("field-height-value").value);
 
   if (turn < 0) {
     turn = document.getElementById("turn-value").value = 0;
@@ -267,53 +259,178 @@ function drawField() {
     turn = document.getElementById("turn-value").value = 1000;
   }
 
-  if (x < 2) {
-    x = document.getElementById("field-width-value").value = 2;
-  } else if (x > 100) {
-    x = document.getElementById("field-width-value").value = 100;
+  if (width < 2) {
+    width = document.getElementById("field-width-value").value = 2;
+  } else if (width > 100) {
+    width = document.getElementById("field-width-value").value = 100;
   }
 
-  if (y < 2) {
-    y = document.getElementById("field-height-value").value = 2;
-  } else if (y > 100) {
-    y = document.getElementById("field-height-value").value = 100;
+  if (height < 2) {
+    height = document.getElementById("field-height-value").value = 2;
+  } else if (height > 100) {
+    height = document.getElementById("field-height-value").value = 100;
   }
 
-  createTable(x, y, turn);
-  // setRobots("csv/sample.csv", 0);
+  createTable();
 }
 
-function setRobots(dataPath, tern) {
-  let robots = [];
+function setContextMenu() {
+  contextMenuObj.length = 0;
+  for (let i = 0; i < robot.length; i++) {
+    var sampleElement = document.getElementById("robot" + i);
+    if (sampleElement == null) break;
+    contextMenuObj[contextMenuObj.length] = new ContextMenu({
+      element: sampleElement,
+      menuList: [
+        {
+          text: "色",
+          action: function () {
+            let str = window.prompt(
+              "設定したい色を教えてください。(例：Red, Green, Blue,...)"
+            );
+            if (str.length != 0) robot[i].setColor(turn, str);
+            drawField();
+          },
+        },
+        {
+          text: "移動",
+          // action: function () {},
+          subMenu: [
+            {
+              text: "直進",
+              action: function () {
+                console.log(i);
+                robot[i].setMove(turn, "Up");
+                drawField();
+              },
+            },
+            {
+              text: "右折",
+              action: function () {
+                robot[i].setMove(turn, "Right");
+                drawField();
+              },
+            },
+            {
+              text: "左折",
+              action: function () {
+                robot[i].setMove(turn, "Left");
+                drawField();
+              },
+            },
+            {
+              text: "後進",
+              action: function () {
+                robot[i].setMove(turn, "Down");
+                drawField();
+              },
+            },
+          ],
+        },
+        {
+          text: "削除",
+          action: function () {
+            robot.splice(i, 1);
+            console.log("削除: %d", i);
+            drawField();
+          },
+        },
+        {
+          text: "向き",
+          action: function () {},
+          subMenu: [
+            {
+              text: "上",
+              action: function () {
+                if (turn == 0) {
+                  robot[i].initialDirection = "Up";
+                  drawField();
+                }
+              },
+            },
+            {
+              text: "右",
+              action: function () {
+                if (turn == 0) {
+                  robot[i].initialDirection = "Right";
+                  drawField();
+                }
+              },
+            },
+            {
+              text: "左",
+              action: function () {
+                if (turn == 0) {
+                  robot[i].initialDirection = "Left";
+                  drawField();
+                }
+              },
+            },
+            {
+              text: "下",
+              action: function () {
+                if (turn == 0) {
+                  robot[i].initialDirection = "Down";
+                  drawField();
+                }
+              },
+            },
+          ],
+        },
+      ],
+    });
+  }
 
-  let request = new XMLHttpRequest(); // HTTPでファイルを読み込む
-  request.addEventListener("load", (event) => {
-    // ロードさせ実行
-    console.log("Load CSV.");
-    const str = event.target.responseText; // 受け取ったテキストを返す
-    // 最終的な二次元配列を入れるための配列
-    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
-
-    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
-    for (var i = 1; i < tmp.length; i++) {
-      let str = [];
-      str[i] = tmp[i].split(",");
-      robots[i - 1] = new Robot(str[i][1], str[i][2], str[i][3]);
-      for (var j = 4; j < str[i].length; ++j) {}
-    }
-
-    console.log(robots);
-  });
-  console.log("%s", dataPath);
-  request.open("GET", dataPath, true); // csvのパスを指定
-  request.send();
-
-  return robots;
+  // for (let i = 1; i <= height; i++) {
+  //   for (let j = 1; j <= width; j++) {
+  //     var sampleElement = document.getElementById("field" + j + "-" + i);
+  //     if (sampleElement == null) {
+  //       break;
+  //     }
+  //     contextMenuObj[contextMenuObj.length] = new ContextMenu({
+  //       element: sampleElement,
+  //       menuList: [
+  //         {
+  //           text: "追加",
+  //           action: function () {
+  //             robot[robot.length] = new Robot(j, i, "Up", "White");
+  //             drawField();
+  //           },
+  //         },
+  //       ],
+  //     });
+  //   }
+  // }
 }
 
-function robotExists(robots, turn, x, y) {
-  for (let i = 0; i < robots.length; i++) {
-    let position = robots[i].position(turn);
+// function placeRobot(turn) {
+//   for (let i = 0; i < robot.length; i++) {
+//     let position = robot[i].position(turn);
+//     console.log("field" + position[0] + "-" + position[1]);
+//     let element = document.getElementById(
+//       "field" + position[0] + "-" + position[1]
+//     );
+//     element.id = "robot" + i;
+//     let direction = robot[robotNumber].direction(turn).toLowerCase();
+//     element.className = "robot_" + direction;
+//     element.innerHTML = robotNumber;
+//   }
+
+//   for (let i = 0; i < robot.length; i++) {
+//     let position = robot[i].position(turn);
+//     let tmp = document.getElementById(
+//       "field" + position[0] + "-" + position[1]
+//     );
+//     tmp.id = "next-robot" + i;
+//     let direction = robot[robotNumber].direction(turn).toLowerCase();
+//     tmp.className = "next_robot_" + direction;
+//     tmp.innerHTML = robotNumber;
+//   }
+// }
+
+function robotExists(turn, x, y) {
+  for (let i = 0; i < robot.length; i++) {
+    let position = robot[i].position(turn);
     // console.log("%d-%d, %d-%d", position[0], x, position[1], y);
     if (position[0] == x && position[1] == y) return i;
   }
@@ -321,11 +438,18 @@ function robotExists(robots, turn, x, y) {
 }
 
 function addRobot() {
-  let str = window.prompt("入力してね");
+  let str = window.prompt("追加するロボットの座標を入力してください(例：5,8)");
+  if (str == null) return;
   str = str.split(",");
   if (str.length == 2) {
-    let x = Number(str[0]);
-    let y = Number(str[1]);
+    let inputX = Number(str[0]);
+    let inputY = Number(str[1]);
+    if (1 <= inputX && inputX <= width && 1 <= inputY && inputY <= height) {
+      if (robotExists(turn, inputX, inputY) == -1) {
+        robot[robot.length] = new Robot(inputX, inputY, "Up", "White");
+        drawField();
+      }
+    }
   }
 }
 
@@ -344,4 +468,82 @@ function directionToNumber(direction) {
     default:
       return -1;
   }
+}
+
+function saveJSON() {
+  let fileName = window.prompt("保存するファイル名を指定してください");
+
+  if (fileName == null) {
+    return;
+  }
+
+  if (fileName.indexOf(".json", 0) == -1) {
+    fileName += ".json";
+    console.log(fileName);
+  }
+
+  // originalDataに，種々のデータが格納されているとする。次は一例。
+  originalData = {
+    field_x: width,
+    field_y: height,
+    robot: [],
+  };
+
+  console.log(originalData);
+
+  for (let i = 0; i < robot.length; i++) {
+    originalData.robot[i] = {};
+    originalData.robot[i].initialX = robot[i].initialPositionX;
+    originalData.robot[i].initialY = robot[i].initialPositionY;
+    originalData.robot[i].initialDirection = robot[i].initialDirection;
+    originalData.robot[i].initialColor = robot[i].initialColor;
+    originalData.robot[i].move = robot[i].move;
+    originalData.robot[i].color = robot[i].color;
+  }
+
+  // データをJSON形式の文字列に変換する。
+  const data = JSON.stringify(originalData);
+
+  // HTMLのリンク要素を生成する。
+  const link = document.createElement("a");
+
+  // リンク先にJSON形式の文字列データを置いておく。
+  link.href = "data:text/plain," + encodeURIComponent(data);
+
+  // 保存するJSONファイルの名前をリンクに設定する。
+  link.download = fileName;
+
+  // ファイルを保存する。
+  link.click();
+}
+
+function readJSON(path) {
+  let xhr = new XMLHttpRequest();
+  let robot = [];
+
+  xhr.onload = () => {
+    let responseJson = JSON.parse(xhr.response);
+
+    document.getElementById("field-width-value").value = responseJson.field_x;
+    document.getElementById("field-height-value").value = responseJson.field_y;
+
+    document.getElementById("number-of-robots").innerHTML =
+      responseJson.robot.length + "台";
+
+    for (let i = 0; i < responseJson.robot.length; i++) {
+      robot[i] = new Robot(
+        responseJson.robot[i].initialX,
+        responseJson.robot[i].initialY,
+        responseJson.robot[i].initialDirection,
+        responseJson.robot[i].initialColor
+      );
+      robot[i].move = responseJson.robot[i].move;
+      robot[i].color = responseJson.robot[i].color;
+    }
+    drawField();
+  };
+  xhr.open("GET", path, true);
+  xhr.send();
+
+  return robot;
 }
